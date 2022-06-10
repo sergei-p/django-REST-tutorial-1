6ed7d021cb24d93a7b8d3700e75a14df58ebcc16
sergei-p <sergeidev2494@gmail.com>
2022-06-10 15:34:22 -0700

    Created new endpoint

--- myproject/api/views.py
+++ myproject/api/views.py
@@ -1,7 +1,19 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from yaml import serialize
+from base.models import Item
+from .serializers import ItemSerializer
 
 @api_view(['GET'])
 def getData(request):
-  person = {'name':'Sergei', 'age': 25}
-  return Response(person)
+  items = Item.objects.all()
+  serializer = ItemSerializer(items, many=True)
+  return Response(serializer.data)
+
+
+@api_view(['POST'])
+def addItem(request):
+  serializer = ItemSerializer(data=request.data) # request.data is the data which was submitted on the fronted
+  if serializer.is_valid():
+    serializer.save()
+  return Response(serializer.data)
